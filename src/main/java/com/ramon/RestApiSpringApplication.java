d6d538f2f77cdffc0bcb3eb0b75ce5a90d83df46
italloramon <irvp@ic.ufal.br>
2023-01-31 21:21:15 -0300

    Non-REST API

--- src/main/java/com/ramon/RestApiSpringApplication.java
+++ src/main/java/com/ramon/RestApiSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiSpringApplication.class, args);
 	}
---
+++ src/main/java/com/ramon/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ramon.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+      super("Could not find employee " + id);
+    }
+  }
